61dab066816dfb1b732f8e48d1ed68909b767776
Javier Naranjo <javier_sbt95@hotmail.com>
2022-12-22 20:23:22 -0500

    GUI tkinter added to rollback script

--- roll_back.py
+++ roll_back.py
@@ -1,18 +1,51 @@
 import os
+from tkinter import *
+from tkinter import filedialog
 
-for carpeta, subcarpetas, archivos in os.walk(os.getcwd()):
-    if carpeta != os.getcwd():
-        for x in archivos:
-            if x.endswith("notes.mid"):
-                print("Configurando: ", os.path.join(carpeta, x))
-                os.remove(os.path.join(carpeta, x))
-            if x.endswith("song.ini"):
-                os.remove(os.path.join(carpeta, x))
+#///////////////////////////////////////////////////////////////////////////////////////
+def CambiarNombre(x, origen, final):
+    items = x.items(origen)
+    x.add_section(final)
+    for item in items:
+        x.set(final, item[0], item[1])
+    x.remove_section(origen)
 
-for carpeta, subcarpetas, archivos in os.walk(os.getcwd()):
-    if carpeta != os.getcwd():
-        for x in archivos:
-                if x.endswith("notes.bak"):
-                    os.rename(os.path.join(carpeta, x), os.path.join(carpeta, "notes.mid"))
-                if x.endswith("song.bak"):
-                    os.rename(os.path.join(carpeta, x), os.path.join(carpeta, "song.ini"))
+#///////////////////////////////////////////////////////////////////////////////////////
+def Explorar():
+    global Elejida
+    directorio = filedialog.askdirectory()
+    if directorio:
+        Elejida.set("Se ha configurado: \n" + directorio)
+        for carpeta, subcarpetas, archivos in os.walk(directorio):
+            if carpeta != directorio:
+                for x in archivos:
+                    if x.endswith("notes.mid"):
+                        print("Configurando: ", os.path.join(carpeta, x))
+                        os.remove(os.path.join(carpeta, x))
+                    if x.endswith("song.ini"):
+                        os.remove(os.path.join(carpeta, x))
+        for carpeta, subcarpetas, archivos in os.walk(directorio):
+            if carpeta != directorio:
+                for x in archivos:
+                    if x.endswith("notes.bak"):
+                        os.rename(os.path.join(carpeta, x), os.path.join(carpeta, "notes.mid"))
+                    if x.endswith("song.bak"):
+                        os.rename(os.path.join(carpeta, x), os.path.join(carpeta, "song.ini"))
+        mensaje.set("DONE! Ya puedes cerrar la ventana")
+
+#///////////////////////////////////////////////////////////////////////////////////////
+root = Tk()
+Elejida = StringVar()
+mensaje = StringVar()
+mensaje.set("Este programa te permite regresar todos los archivos modificados \na su estado original...")
+lbl1 = Label(master=root,textvariable=Elejida)
+lbl1.grid(row=2, column=0)
+lbl2 = Label(master=root,textvariable=mensaje)
+lbl2.grid(row=0, column=0)
+b_explorar = Button(text="Buscar Capeta", command=Explorar)
+b_explorar.grid(row=1, column=0)
+Lower_left = Label(root,text ='Silly Python Script by J.Naranjo 2022')
+Lower_left.place(relx = 0.0, rely = 1.0, anchor ='sw')
+root.geometry("450x300")
+root.title('UNDO Video Start Time (Clone Hero)')
+mainloop()
